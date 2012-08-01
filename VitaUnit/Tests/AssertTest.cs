978d85c81e62587c4de1a29c96b9cb3ca6b14748
Gary Johnson <gary@gjtt.com>
2012-07-31 20:53:39 -0400

    Implementing Assert.AreEqual

--- VitaUnit/Tests/AssertTest.cs
+++ VitaUnit/Tests/AssertTest.cs
@@ -47,7 +47,7 @@ namespace VitaUnit.Test
 			bool threwException = false;
 			
 			try {
-				Assert.AreEqual(1,2);
+				Assert.AreEqual(1, 2);
 			} catch (VitaUnitException) {
 				threwException = true;
 			}
@@ -58,6 +58,33 @@ namespace VitaUnit.Test
 		[TestMethod]
 		public void ShouldNotThrowExceptionWhenAssertAreEqualSucceedsForValueType() {
 			Assert.AreEqual(100, 100);
+		}
+				
+		[TestMethod]
+		public void ShouldThrowVitaUnitExceptionWhenAssertAreEqualFailsForReferenceType() {
+			
+			bool threwException = false;
+			
+			try {
+				object testObject1 = new object();
+				object testObject2 = new object();
+				Assert.AreEqual(testObject1, testObject2);
+			} catch (VitaUnitException) {
+				threwException = true;
+			}
+			
+			Assert.IsTrue(threwException);
+		}
+		
+		[TestMethod]
+		public void ShouldNotThrowExceptionWhenAssertAreEqualSucceedsForReferenceType() {
+			object testObject = new object();
+			Assert.AreEqual(testObject, testObject);
+		}
+		
+		[TestMethod]
+		public void ShouldNotThrowExceptionWhenAssertAreEqualGetsNulls() {
+			Assert.AreEqual(null, null);
 		}
 	}
 }
